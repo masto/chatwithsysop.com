2aebc387244e5fb53521d9b82eeced6fb99dae96
Christopher Masto <chris@masto.com>
2025-01-05 14:52:34 -0500

    Clean up the home page override plugin

--- plugins/blog-plugin.js
+++ plugins/blog-plugin.js
@@ -1,53 +1,53 @@
+// This plugin extends the default Docusaurus blog plugin to add a home page
+// that displays the latest blog posts.
+
 // Thanks: https://kgajera.com/blog/display-recent-blog-posts-on-home-page-with-docusaurus/
-// See alse: https://github.com/facebook/docusaurus/issues/4138#issuecomment-1703083513
+// See also: https://github.com/facebook/docusaurus/issues/4138#issuecomment-1703083513
 
-const blogPluginExports = require("@docusaurus/plugin-content-blog");
+import defaultBlogPlugin from "@docusaurus/plugin-content-blog";
 
-const defaultBlogPlugin = blogPluginExports.default;
+export default async function blogPluginExtended(...pluginArgs) {
+  const blogPluginInstance = await defaultBlogPlugin(...pluginArgs);
 
-async function blogPluginExtended(...pluginArgs) {
-    const blogPluginInstance = await defaultBlogPlugin(...pluginArgs);
+  return {
+    // Add all properties of the default blog plugin so existing functionality
+    // is preserved.
+    ...blogPluginInstance,
 
-    return {
-        // Add all properties of the default blog plugin so existing functionality is preserved
-        ...blogPluginInstance,
-        /**
-         * Override the default `contentLoaded` hook to access blog posts data
-         */
-        contentLoaded: async function (data) {
-            // Get the latest blog posts
-            const recentPosts = [...data.content.blogPosts].splice(0, 5);
+    // Custom home page that contains the latest blog posts.
+    contentLoaded: async function (data) {
+      // Get the latest blog posts
+      const recentPosts = [...data.content.blogPosts].splice(0, 5);
 
-            data.actions.addRoute({
-                // Add route for the home page
-                path: "/",
-                exact: true,
+      data.actions.addRoute({
+        // Add route for the home page
+        path: "/",
+        exact: true,
 
-                // The component to use for the "Home" page route
-                component: "@site/src/components/Home",
+        // The component to use for the home page route
+        component: "@site/src/components/Home",
 
-                // These are the props that will be passed to our "Home" page component
-                modules: {
-                    recentPosts: recentPosts.map((post) => ({
-                        content: {
-                            __import: true,
-                            // The markdown file for the blog post will be loaded by webpack
-                            path: post.metadata.source,
-                            query: {
-                                truncated: true,
-                            },
-                        },
-                    })),
-                },
-            });
-
-            // Call the default overridden `contentLoaded` implementation
-            return blogPluginInstance.contentLoaded(data);
+        // These are the props that will be passed to our "Home" page component
+        modules: {
+          recentPosts: recentPosts.map((post) => ({
+            content: {
+              __import: true,
+              // The markdown file for the blog post will be loaded by webpack
+              path: post.metadata.source,
+              query: {
+                truncated: true,
+              },
+            },
+          })),
         },
-    };
+      });
+
+      // Call the base `contentLoaded` implementation to generate the standard
+      // blog pages.
+      return blogPluginInstance.contentLoaded(data);
+    },
+  };
 }
 
-module.exports = {
-    ...blogPluginExports,
-    default: blogPluginExtended,
-};
+// Export everything else from the default blog plugin for compatibility.
+export * from "@docusaurus/plugin-content-blog";
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,60 +1,48 @@
-import React from "react";
-import clsx from 'clsx';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from "@theme/Layout";
-import BlogPostItem from "@theme/BlogPostItem";
-import PaginatorNavLink from "@theme/PaginatorNavLink";
 import { BlogPostProvider } from "@docusaurus/plugin-content-blog/client";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import BlogPostItem from "@theme/BlogPostItem";
+import Layout from "@theme/Layout";
+import clsx from "clsx";
+import React from "react";
 
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
 
 function HomepageHeader() {
-    const { siteConfig } = useDocusaurusContext();
-    return (
-        <header className={clsx('hero hero--primary', styles.heroBanner)}>
-            <div className="container">
-                <h1 className="hero__title">{siteConfig.title}</h1>
-                <p className="hero__subtitle">{siteConfig.tagline}</p>
-            </div>
-        </header>
-    );
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+      </div>
+    </header>
+  );
 }
 
 export default function Home({ recentPosts }) {
-    const { siteConfig } = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
 
-    return (
-        <Layout
-            title={`${siteConfig.title}`}
-            description="Personal web site">
-            <HomepageHeader />
-            <main>
-                <div className="container margin-top--xl margin-bottom--lg">
-                    <div className="row">
-                        <div className="col col--9 col--offset-1">
-                            {recentPosts.map(({ content: BlogPostContent }) => (
-                                <BlogPostProvider
-                                    key={BlogPostContent.metadata.permalink}
-                                    content={BlogPostContent}
-                                >
-                                    <BlogPostItem>
-                                        <BlogPostContent />
-                                    </BlogPostItem>
-                                </BlogPostProvider>
-                            ))}
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col col--5 col--offset-5">
-                            {/* <PaginatorNavLink
-                                isNext
-                                permalink="/blog/page/2"
-                                title="Older Entries"
-                            /> */}
-                        </div>
-                    </div>
-                </div>
-            </main>
-        </Layout>
-    );
-};
+  return (
+    <Layout title={`${siteConfig.title}`} description="Chris Masto's web site">
+      <HomepageHeader />
+      <main>
+        <div className="container margin-top--xl margin-bottom--lg">
+          <div className="row">
+            <div className="col col--9 col--offset-1">
+              {recentPosts.map(({ content: BlogPostContent }) => (
+                <BlogPostProvider
+                  key={BlogPostContent.metadata.permalink}
+                  content={BlogPostContent}
+                >
+                  <BlogPostItem>
+                    <BlogPostContent />
+                  </BlogPostItem>
+                </BlogPostProvider>
+              ))}
+            </div>
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
+}
